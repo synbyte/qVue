03e7f6a795504703ff3bfd5dfd06685464a79d82
synbyte <rashnikov2.0@gmail.com>
2022-10-09 15:16:19 -0700

    initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page class="flex flex-center">
+ <q-btn to="/upload"><q-icon name="upload"></q-icon>Upload Photos</q-btn>
+ <span></span>
+   
+  </q-page>
+  <!--<q-dialog v-model="dia">     
+              <q-uploader  url='http://localhost:4444/upload'></q-uploader>
+</q-dialog>        -->      
+  
+ 
+    </template>
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup(){
+   return {
+       dia:ref(true)
+   }   
+  },
+  onMounted(){
+      $q.fullscreen.request()
+  }
+
+})
+</script>
---
+++ src/pages/UploadItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+     <> 
+  <div class="q-pa-md"><q-form><q-input label="Im looking for.."> </q-input></q-form></div>
+  </q-page>
+ 
+
+    </template>
+
+<script>
+import { defineComponent, ref, onMounted } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup(){
+   return {
+       dia:ref(true)
+   }   
+  },
+  onMounted(){
+      $q.fullscreen.request()
+  }
+
+})
+</script>
