0e17bc874c619625ab2ee3571ed04edc582b51ab
synbyte <rashnikov2.0@gmail.com>
2022-10-09 18:27:57 -0700

    econd

--- src/pages/UploadItem.vue
+++ src/pages/UploadItem.vue
@@ -1,7 +1,8 @@
 <template>
   <q-page class="flex flex-center">
-     <> 
-  <div class="q-pa-md"><q-form><q-input label="Im looking for.."> </q-input></q-form></div>
+    <q-card class="full-height"> 
+  <div class="q-pa-md"><q-form><q-input label="Im looking for.."> </q-input><q-dialog v-model="dia"><q-upload url="http://localhost:5000">test</q-upload></q-dialog
+  ><q-btn @click="dia = true"><q-icon name="upload"></q-icon>Upload Photos</q-btn></q-form></div></q-card>
   </q-page>
  
 
@@ -14,7 +15,7 @@ export default defineComponent({
   name: 'IndexPage',
   setup(){
    return {
-       dia:ref(true)
+       dia:ref(false)
    }   
   },
   onMounted(){
